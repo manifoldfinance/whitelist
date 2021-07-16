fc24b802ab272b645159b53e1c4262ba89150656
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2021-07-16 15:31:25 +0200

    feat: updated allow list for manifold

--- whitelists/manifoldfinance/build.js
+++ whitelists/manifoldfinance/build.js
@@ -7,13 +7,16 @@ const FILE_NAMES = {
   [ChainId.RINKEBY]: "rinkeby",
   [ChainId.GÖRLI]: "goerli",
   [ChainId.KOVAN]: "kovan",
-  [ChainId.FANTOM]: "fantom",
-  [ChainId.FANTOM_TESTNET]: "fantom-testnet",
   [ChainId.MATIC]: "matic",
   [ChainId.MATIC_TESTNET]: "matic-testnet",
+  [ChainId.FANTOM]: "fantom",
+  [ChainId.FANTOM_TESTNET]: "fantom-testnet",
   [ChainId.XDAI]: "xdai",
   [ChainId.BSC]: "bsc",
   [ChainId.BSC_TESTNET]: "bsc-testnet",
+  [ChainId.ARBITRUM]: "arbitrum",
+  [ChainId.ARBITRUM_TESTNET]: "arbitrum-testnet",
+  [ChainId.MOONBEAM_TESTNET]: "moonbean-testnet",
   [ChainId.AVALANCHE]: "avalanche",
   [ChainId.AVALANCHE_TESTNET]: "avalanche-testnet",
   [ChainId.HECO]: "heco",
@@ -22,6 +25,7 @@ const FILE_NAMES = {
   [ChainId.HARMONY_TESTNET]: "harmony-testnet",
   [ChainId.OKEX]: "okex",
   [ChainId.OKEX_TESTNET]: "okex-testnet",
+  [ChainId.CELO]: "celo",
 };
 
 const tokens = Object.keys(ChainId).reduce((previousValue, currentValue) => {
